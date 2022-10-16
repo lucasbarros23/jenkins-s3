327ba73cef33483358975d5acf2d6ce4ae229bd9
lucasbarros23 <70347936+lucasbarros23@users.noreply.github.com>
2022-10-16 16:27:10 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,13 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "bucket-s3-ahmlkq-hehe"
-  acl    = "private"
+# --- root/main.tf ---
 
-  tags = {
-    Name        = "INFORME-UMA-TAG-PARA-BUCKET"
-    Environment = "Dev"
-  }
+module "networking" {
+  source       = "./networking"
+  vpc_cidr     = "10.0.0.0/16"
+  public_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+module "compute" {
+  source        = "./compute"
+  web_sg        = module.networking.web_sg
+  public_subnet = module.networking.public_subnet
 }
