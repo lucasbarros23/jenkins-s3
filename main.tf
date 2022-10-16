497e23d7681f8f5c8c88d3d5434d91ae96d4688b
lucasbarros23 <70347936+lucasbarros23@users.noreply.github.com>
2022-10-16 16:35:51 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,4 @@
-# --- root/main.tf ---
-
-module "networking" {
-  source       = "./networking"
-  vpc_cidr     = "10.0.0.0/16"
-  public_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
-}
-
-module "compute" {
-  source        = "./compute"
-  web_sg        = module.networking.web_sg
-  public_subnet = module.networking.public_subnet
-}
+  backend "s3" {
+    bucket = "great-name-pilsen-terraform-state-2"
+    region = "eu-central-1"
+    }
